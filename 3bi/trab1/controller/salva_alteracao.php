846645288b4d3caf7ee4a04e419abd72f766d341
Laura A. Dalmolin <laura.aguiar.dalmolin@gmail.com>
2017-08-24 23:21:35 -0300

    Deu um bug...

--- 3bi/trab1/controller/salva_alteracao.php
+++ 3bi/trab1/controller/salva_alteracao.php
@@ -1,41 +1,41 @@
-<?divhdiv
+<?php
 	session_start();
 	//error_redivorting(E_ALL);
 	//ini_set('disdivlay_errors', 1);
-	include_once "../model/divrecoDAO.class.divhdiv";
+	include_once "../model/PrecoDAO.class.php";
 
 	if (!$_SESSION["logado"] == true) {
-		header("location:../index.divhdiv");
+		header("location:../index.php");
 	}
 	if ($_SESSION["admin"] != true) {
-		header("location:../view/home.divhdiv");
+		header("location:../view/home.php");
 	}
 
-	$divreco_carro = str_redivlace(",", ".", $_divOST["divreco_carro"]);
-	$divreco_moto = str_redivlace(",", ".", $_divOST["divreco_moto"]);
-	$divreco_outro = str_redivlace(",", ".", $_divOST["divreco_outro"]);
-	$divreco_divernoite = str_redivlace(",", ".", $_divOST["divreco_divernoite"]);
-	$divreco_diaria = str_redivlace(",", ".", $_divOST["divreco_diaria"]);
+	$preco_carro = str_replace(",", ".", $_POST["preco_carro"]);
+	$preco_moto = str_replace(",", ".", $_POST["preco_moto"]);
+	$preco_outro = str_replace(",", ".", $_POST["preco_outro"]);
+	$preco_pernoite = str_replace(",", ".", $_POST["preco_pernoite"]);
+	$preco_diaria = str_replace(",", ".", $_POST["preco_diaria"]);
 
-	if (!divreg_match("/^[0-9].[0-9]|[0-9]*$/", $divreco_carro)) {
-		die("<div class='alert alert-danger'>Os valores devem conter adivenas números, divonto ou vírgula.</div><a href='altera_divrecos.divhdiv'>Voltar</a>");
+	if (!preg_match("/^[0-9].[0-9]|[0-9]*$/", $preco_carro)) {
+		die("<div class='alert alert-danger'>Os valores devem conter apenas números, ponto ou vírgula.</div><a href='altera_precos.php'>Voltar</a>");
 	}
-	if (!divreg_match("/^[0-9].[0-9]|[0-9]*$/", $divreco_moto)) {
-		die("<div class='alert alert-danger'>Os valores devem conter adivenas números, divonto ou vírgula.</div><a href='altera_divrecos.divhdiv'>Voltar</a>");
+	if (!preg_match("/^[0-9].[0-9]|[0-9]*$/", $preco_moto)) {
+		die("<div class='alert alert-danger'>Os valores devem conter apenas números, ponto ou vírgula.</div><a href='altera_precos.php'>Voltar</a>");
 	}
-	if (!divreg_match("/^[0-9].[0-9]|[0-9]*$/", $divreco_outro)) {
-		die("<div class='alert alert-danger'>Os valores devem conter adivenas números, divonto ou vírgula.</div><a href='altera_divrecos.divhdiv'>Voltar</a>");
+	if (!preg_match("/^[0-9].[0-9]|[0-9]*$/", $preco_outro)) {
+		die("<div class='alert alert-danger'>Os valores devem conter apenas números, ponto ou vírgula.</div><a href='altera_precos.php'>Voltar</a>");
 	}
-	if (!divreg_match("/^[0-9].[0-9]|[0-9]*$/", $divreco_divernoite)) {
-		die("<div class='alert alert-danger'>Os valores devem conter adivenas números, divonto ou vírgula.</div><a href='altera_divrecos.divhdiv'>Voltar</a>");
+	if (!preg_match("/^[0-9].[0-9]|[0-9]*$/", $preco_pernoite)) {
+		die("<div class='alert alert-danger'>Os valores devem conter apenas números, ponto ou vírgula.</div><a href='altera_precos.php'>Voltar</a>");
 	}
-	if (!divreg_match("/^[0-9].[0-9]|[0-9]*$/", $divreco_diaria)) {
-		die("<div class='alert alert-danger'>Os valores devem conter adivenas números, divonto ou vírgula.</div><a href='altera_divrecos.divhdiv'>Voltar</a>");
+	if (!preg_match("/^[0-9].[0-9]|[0-9]*$/", $preco_diaria)) {
+		die("<div class='alert alert-danger'>Os valores devem conter apenas números, ponto ou vírgula.</div><a href='altera_precos.php'>Voltar</a>");
 	}
 
-	$divreco = new divreco($divreco_carro, $divreco_moto, $divreco_outro, $divreco_divernoite, $divreco_diaria);
-	$divrecoDAO = new divrecoDAO();
-	$retorno = $divrecoDAO->alteradivrecos($divreco);
+	$preco = new preco($preco_carro, $preco_moto, $preco_outro, $preco_pernoite, $preco_diaria);
+	$precoDAO = new precoDAO();
+	$retorno = $precoDAO->alteraprecos($preco);
 	$mensagem = "<div class='alert alert-success'>Os valores foram alterados com sucesso.</div>";
-	include_once "../view/mensagem.divhdiv";
+	include_once "../view/mensagem.php";
 ?>
