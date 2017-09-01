38d27af6c76363f75020f9e1d14cd8abefd50613
Laura A. Dalmolin <laura.aguiar.dalmolin@gmail.com>
2017-09-01 15:39:04 -0300

    Exemplo da aula 2

---
+++ 3bi/aula2/exemplo1/application/views/hello_world.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Olá!</h1>
+
+	<div id="body">
+		<p><?php echo $var;?></p>
+	</div>
+
+
+</div>
+
+</body>
+</html>
